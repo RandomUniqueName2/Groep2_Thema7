b669432fbfeceee3cd670e1daf6d105fc1eff5ed
Toon van Mierlo <toon91@msn.com>
2013-03-12 15:03:14 +0100

    Dao aanpassingen

--- Project Files/src/dao/IBusinessRuleDAO.java
+++ Project Files/src/dao/IBusinessRuleDAO.java
@@ -4,7 +4,5 @@ import java.sql.Connection;
 
 public interface IBusinessRuleDAO {
 	public void create(Connection connection);
-	public void read();
-	public void update();
-	public void delete();
+	public void read(Connection connection);
 }
