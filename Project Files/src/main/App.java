ed35c63d18320fe1cc70734be2bbf4781b849d36
Rándom-Portable <randomuniquenamefakeemail001@randomfake.com>
2013-03-14 12:49:53 +0100

    Revert "random?"
    This reverts commit d449e98091be755a832efa06226d779fb3ecf478.

---
+++ Project Files/src/main/App.java
@@ -0,0 +1,12 @@
+package main;
+
+import gui.GeneratorMainFrame;
+
+public class App {	
+	
+	public static GeneratorMainFrame mainFrame = null;
+	
+	public static void main(String[] args) {
+		mainFrame = new GeneratorMainFrame();
+	}
+}
